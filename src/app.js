159c767e56921b311532b509f345355e59f68781
Deives Fahl <dfahl.cps@gmail.com>
2021-12-22 20:08:24 -0300

    Adicionado pacote cors e alterado inicializador para o arquivo server.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
-require('dotenv').config();
-
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+const routesSite = require('./routes/site');
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-app.use('/', require('./routes/site'));
-app.use('/about', require('./routes/site'));
-app.use('/contact', require('./routes/site'));
+app.use(cors());
 
-app.listen(process.env.PORT || 3000);
+app.use('/', routesSite);
+app.use('/about', routesSite);
+app.use('/contact', routesSite);
+
+module.exports = { app };
